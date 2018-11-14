b14656a1fe0cb8968ceea19b1f9bc49271bd16ee
Natalia <belka531@mail.ru>
2018-11-14 12:10:55 +0100

    bla

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
+const companiesRouter = require('./companies/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -9,4 +10,7 @@ app
   .use(bodyParser.json())
   .use(customersRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
+
+
+
 
